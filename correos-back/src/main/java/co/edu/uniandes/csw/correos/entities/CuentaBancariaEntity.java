8aa6f39b5c7f8fb2b3a501cea6b59bd4e6d62413
a.silvag <a.silvag@ISIS2603S2-0066.sis.virtual.uniandes.edu.co>
2018-02-17 10:45:01 -0500

    Las entities terminadas

---
+++ correos-back/src/main/java/co/edu/uniandes/csw/correos/entities/CuentaBancariaEntity.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.correos.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author a.silvag
+ */
+public class CuentaBancariaEntity extends BaseEntity implements Serializable{
+
+    private Long id;
+    private String numero;
+    private String banco;
+    private String tipoTarjeta;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the numero
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the banco
+     */
+    public String getBanco() {
+        return banco;
+    }
+
+    /**
+     * @param banco the banco to set
+     */
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    /**
+     * @return the tipoTarjeta
+     */
+    public String getTipoTarjeta() {
+        return tipoTarjeta;
+    }
+
+    /**
+     * @param tipoTarjeta the tipoTarjeta to set
+     */
+    public void setTipoTarjeta(String tipoTarjeta) {
+        this.tipoTarjeta = tipoTarjeta;
+    }
+
+}
