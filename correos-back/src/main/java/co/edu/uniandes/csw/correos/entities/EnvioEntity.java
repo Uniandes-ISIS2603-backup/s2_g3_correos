b754a85775d6a948cf9ca727b44124249b96a17f
boleointenso <36081822+boleointenso@users.noreply.github.com>
2018-02-17 13:29:52 -0500

    Entity Envio||Paquete
    Faltan las asociaciones

---
+++ correos-back/src/main/java/co/edu/uniandes/csw/correos/entities/EnvioEntity.java
@@ -0,0 +1,126 @@
+/*
+MIT License
+
+Copyright (c) 2017 ISIS2603
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package co.edu.uniandes.csw.correos.entities;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author df.rengifo
+ */
+public class EnvioEntity extends BaseEntity implements Serializable{    
+    
+   private Long id;
+   private Long horaInicio;
+   private Long horaFinal;    
+   private String estado;
+   private String direccionEntrega;
+   private String direccionRecogida;
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the horaInicio
+     */
+    public Long getHoraInicio() {
+        return horaInicio;
+    }
+
+    /**
+     * @param horaInicio the horaInicio to set
+     */
+    public void setHoraInicio(Long horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    /**
+     * @return the horaFinal
+     */
+    public Long getHoraFinal() {
+        return horaFinal;
+    }
+
+    /**
+     * @param horaFinal the horaFinal to set
+     */
+    public void setHoraFinal(Long horaFinal) {
+        this.horaFinal = horaFinal;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the direccionEntrega
+     */
+    public String getDireccionEntrega() {
+        return direccionEntrega;
+    }
+
+    /**
+     * @param direccionEntrega the direccionEntrega to set
+     */
+    public void setDireccionEntrega(String direccionEntrega) {
+        this.direccionEntrega = direccionEntrega;
+    }
+
+    /**
+     * @return the direccionRecogida
+     */
+    public String getDireccionRecogida() {
+        return direccionRecogida;
+    }
+
+    /**
+     * @param direccionRecogida the direccionRecogida to set
+     */
+    public void setDireccionRecogida(String direccionRecogida) {
+        this.direccionRecogida = direccionRecogida;
+    }
+
+     
+}
