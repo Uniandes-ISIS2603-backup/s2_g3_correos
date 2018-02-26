daa10f3a18c5ff25d621045ff41cc63f5f02dbd9
boleointenso <df.rengifo@uniandes.edu.co>
2018-02-26 11:40:09 -0500

    Asociaciones en las Entities

--- correos-back/src/main/java/co/edu/uniandes/csw/correos/entities/EnvioEntity.java
+++ correos-back/src/main/java/co/edu/uniandes/csw/correos/entities/EnvioEntity.java
@@ -7,7 +7,12 @@
 package co.edu.uniandes.csw.correos.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
 
 /**
  *
@@ -20,8 +25,33 @@ public class EnvioEntity extends BaseEntity implements Serializable{
    private Long horaFinal;    
    private String estado;
    private String direccionEntrega;
-   private String direccionRecogida;  
+   private String direccionRecogida; 
+   
+   @PodamExclude
+   @ManyToOne
+   private MensajeroEntity mensajero;
+   
+   @PodamExclude
+   @ManyToOne
+   private ClienteEntity cliente;
+   
+   @PodamExclude
+   @ManyToOne
+   private PagoEntity pago;
+   
+   @PodamExclude
+   @OneToMany(mappedBy = "eventos")
+   private List<EventoEntity> eventos = new ArrayList<EventoEntity>();
+   
+   @PodamExclude
+   @OneToMany(mappedBy = "paquetes")
+   private List<PaqueteEntity> paquetes = new ArrayList<PaqueteEntity>();
+   
+   @PodamExclude
+   @OneToMany(mappedBy = "paquetes")
+   private List<BonoEntity> bonos = new ArrayList<BonoEntity>();
 
+   
     /**
      * @return the horaInicio
      */
@@ -91,4 +121,88 @@ public class EnvioEntity extends BaseEntity implements Serializable{
     public void setDireccionRecogida(String direccionRecogida) {
         this.direccionRecogida = direccionRecogida;
     }     
+
+    /**
+     * @return the mensajero
+     */
+    public MensajeroEntity getMensajero() {
+        return mensajero;
+    }
+
+    /**
+     * @param mensajero the mensajero to set
+     */
+    public void setMensajero(MensajeroEntity mensajero) {
+        this.mensajero = mensajero;
+    }
+
+    /**
+     * @return the cliente
+     */
+    public ClienteEntity getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(ClienteEntity cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * @return the pago
+     */
+    public PagoEntity getPago() {
+        return pago;
+    }
+
+    /**
+     * @param pago the pago to set
+     */
+    public void setPago(PagoEntity pago) {
+        this.pago = pago;
+    }
+
+    /**
+     * @return the eventos
+     */
+    public List<EventoEntity> getEventos() {
+        return eventos;
+    }
+
+    /**
+     * @param eventos the eventos to set
+     */
+    public void setEventos(List<EventoEntity> eventos) {
+        this.eventos = eventos;
+    }
+
+    /**
+     * @return the paquetes
+     */
+    public List<PaqueteEntity> getPaquetes() {
+        return paquetes;
+    }
+
+    /**
+     * @param paquetes the paquetes to set
+     */
+    public void setPaquetes(List<PaqueteEntity> paquetes) {
+        this.paquetes = paquetes;
+    }
+
+    /**
+     * @return the bonos
+     */
+    public List<BonoEntity> getBonos() {
+        return bonos;
+    }
+
+    /**
+     * @param bonos the bonos to set
+     */
+    public void setBonos(List<BonoEntity> bonos) {
+        this.bonos = bonos;
+    }
 }
