0d7ad5e74839dd697ff1f1c41fed8653740ab250
ed.diaz11 <ed.diaz11@ISIS2603S2-0086.sis.virtual.uniandes.edu.co>
2018-02-27 11:28:09 -0500

    Calificacion3

--- correos-web/src/main/java/co/edu/uniandes/csw/correos/dtos/CalidicacionDetailDTO.java
+++ correos-web/src/main/java/co/edu/uniandes/csw/correos/dtos/CalidicacionDetailDTO.java
@@ -9,6 +9,6 @@ package co.edu.uniandes.csw.correos.dtos;
  *
  * @author ed.diaz11
  */
-public class CalidicaionDetailDTO  extends CalificacionDTO {
+public class CalidicacionDetailDTO  extends CalificacionDTO {
     
 }
