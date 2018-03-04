e67ab580aab47d834d18aeb0e028aa4203b75e48
a.silvag <a.silvag@ISIS2603S2-0066.sis.virtual.uniandes.edu.co>
2018-03-04 11:26:22 -0500

    Se sumo la etiqueta de run with arquillian

--- correos-back/src/test/java/co/edu/uniandes/csw/corres/test/logic/EventoLogicTest.java
+++ correos-back/src/test/java/co/edu/uniandes/csw/corres/test/logic/EventoLogicTest.java
@@ -17,6 +17,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
@@ -26,6 +27,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
@@ -33,6 +35,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
  *
  * @author a.silvag
  */
+@RunWith(Arquillian.class)
 public class EventoLogicTest {
     
       private PodamFactory factory = new PodamFactoryImpl();
