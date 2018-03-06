ec7e63f26c67da467fd5847899125c5f9384cebf
boleointenso <df.rengifo@uniandes.edu.co>
2018-03-06 11:10:55 -0500

    pull master

--- correos-back/src/main/java/co/edu/uniandes/csw/correos/ejb/PaqueteLogic.java
+++ correos-back/src/main/java/co/edu/uniandes/csw/correos/ejb/PaqueteLogic.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package co.edu.uniandes.csw.correos.ejb;
 
 import co.edu.uniandes.csw.correos.entities.PaqueteEntity;
