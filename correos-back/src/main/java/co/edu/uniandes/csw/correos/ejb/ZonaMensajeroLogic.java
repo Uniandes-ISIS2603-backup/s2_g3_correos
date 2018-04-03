fdddcfea9f55d864f70fdd54ce864df28586ef76
l.mejia <l.mejia@ISIS2603S2-0061.sis.virtual.uniandes.edu.co>
2018-04-03 10:54:57 -0500

    Arreglo en un fallo que no permitia agregar fechas a las reservas desde postman Arreglo en un fallo que no permitia que se agregaran mensajeros a una zona y viceversa Otros cuantos fixes!

---
+++ correos-back/src/main/java/co/edu/uniandes/csw/correos/ejb/ZonaMensajeroLogic.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.correos.ejb;
+
+import co.edu.uniandes.csw.correos.entities.MensajeroEntity;
+import co.edu.uniandes.csw.correos.entities.ZonaEntity;
+import co.edu.uniandes.csw.correos.persistence.MensajeroPersistence;
+import co.edu.uniandes.csw.correos.persistence.ZonaPersistence;
+import java.util.List;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author l.mejia
+ */
+public class ZonaMensajeroLogic {
+    
+    ZonaPersistence zonaPersistence;
+    MensajeroPersistence mesnajeroPersistence;
+    
+    ZonaLogic zonaLogic;
+    MensajeroLogic mensajeroLogic;
+    
+    @Inject
+    public ZonaMensajeroLogic(ZonaPersistence zonaPersistence, MensajeroPersistence mensajeroPersistence, ZonaLogic zonaLogic, MensajeroLogic mensajeoLogic)
+    {
+        this.zonaPersistence=zonaPersistence;
+        this.mesnajeroPersistence=mensajeroPersistence;
+        
+        this.mensajeroLogic=mensajeoLogic;
+        this.zonaLogic=zonaLogic;
+    }
+    
+    public ZonaMensajeroLogic()
+    {
+        zonaPersistence=null;
+        zonaLogic=null;
+        
+        mesnajeroPersistence=null;
+        mensajeroLogic=null;
+    }
+    
+    public void agregarRelacion(MensajeroEntity mensajero, ZonaEntity zona)
+    {
+        MensajeroEntity mensajeroAgregar=mensajeroLogic.getMensajero(mensajero.getId());
+        ZonaEntity zonaAgregar= zonaLogic.getZona(zona.getId());
+        
+        if(mensajeroAgregar!=null && zonaAgregar!=null)
+        {
+            List<ZonaEntity> zonas=mensajeroAgregar.getZonas();
+            zonas.add(zona);
+            mensajeroAgregar.setZonas(zonas);
+            List<MensajeroEntity> mensajeros=zonaAgregar.getMensajeros();
+            mensajeros.add(mensajero);
+            zonaAgregar.setMensajeros(mensajeros);
+            mesnajeroPersistence.update(mensajeroAgregar);
+            zonaPersistence.update(zonaAgregar);
+        }
+    }
+    
+}
