9392fdd59fd5163675db707163d94ec86102419d
l.mejia <l.mejia@ISIS2603S2-0061.sis.virtual.uniandes.edu.co>
2018-04-12 11:20:45 -0500

    Cambios

--- correos-web/src/main/webapp/src/modules/reservas/reservas.mod.js
+++ correos-web/src/main/webapp/src/modules/reservas/reservas.mod.js
@@ -1,58 +1,60 @@
 /**
  * @ngdoc overview
- * @name transportes.module:transporteModule
+ * @name reservas.module:reservaModule
  * @description
  * Definición del módulo de Angular de Editorial. El módulo encapsula todos los 
- * controladores y los templates HTML que estén relacionados con el transporte 
+ * controladores y los templates HTML que estén relacionados con el reserva 
  * directamente. En la configuración del módulo se injecta la dependencia de 
  * ui.router que es la que se utiliza para la configuración de las URLs bajo las
  * cuales se accede al módulo. Por ejemplo, para mostrar las editoriales en la 
- * URL: 'localhost:8080/transportes/list' es necesario configurar el router por 
+ * URL: 'localhost:8080/reservas/list' es necesario configurar el router por 
  * medio del stateProvider que informa a AngularJS de la relación entre la URL, 
  * un estado definido (estado de mostrar transportes), el controlador y la vista 
  * correspondiente. Los estados definidos en este modulo son:
  * ```
  * | ESTADO          | URL                        | VISTAS                 |
  * |-----------------|----------------------------|------------------------|
- * | transportes     | /transportes               | mainView:              |
- * |                 |                            | transportes.html       |
+ * | reservas        | /reservas                  | mainView:              |
+ * |                 |                            | reservas.html          |
  * |                 |                            |                        |
- * | transportesList | /list                      | listView:              |
- * |                 |                            | transportes.list.html  |
+ * | reservasList    | /list                      | listView:              |
+ * |                 |                            | reservas.list.html     |
  * |                 |                            |                        |
  * |-----------------|----------------------------|------------------------|
  *```
  */
 (function (ng) {
-    var mod = ng.module("transportesModule", ['mensajerosModule', 'ui.router']);
-    mod.constant("transportesContext", "transportes");
+    var mod = ng.module("reservasModule", ['mensajerosModule', 'ui.router']);
+    mod.constant("reservasContext", "reservas");
     mod.constant("mensajerosContext", "api/mensajeros");
 
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            var basePath = 'src/modules/transportes/';
-            $urlRouterProvider.otherwise("/transportesList");
+            var basePath = 'src/modules/reservas/';
+            $urlRouterProvider.otherwise("/reservasList");
 
-            $stateProvider.state('transportes', {
-                url: '/transportes',
+            $stateProvider.state('reservas', {
+                url: '/reservas',
                 abstract: true,
                 parent: 'mensajeroDetail',
                 views: {
                     childrenView: {
-                        templateUrl: basePath + 'transportes.html'
+                        templateUrl: basePath + 'reservas.html'
                     }
                 }
-            }).state('transportesList', {
+            }).state('reservasList', {
                 url: '/list',
-                parent: 'transportes',
+                parent: 'reservas',
                 views: {
                     'listView': {
-                        templateUrl: basePath + 'transportes.list.html',
-                        controller: 'transporteCtrl',
+                        templateUrl: basePath + 'reservas.list.html',
+                        controller: 'reservaCtrl',
                         controllerAs: 'ctrl'
                     }
                 }
             });
         }]);
 })(window.angular);
+
+
 
 
