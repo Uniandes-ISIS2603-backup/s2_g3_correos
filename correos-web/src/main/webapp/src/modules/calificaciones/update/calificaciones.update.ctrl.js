5d4981917e69fc15bb5efb61442a24b4a9daf865
ed.diaz11 <ed.diaz11@ISIS2603S2-0086.sis.virtual.uniandes.edu.co>
2018-04-17 17:24:30 -0500

    Calificacion CRU

---
+++ correos-web/src/main/webapp/src/modules/calificaciones/update/calificaciones.update.ctrl.js
@@ -0,0 +1,67 @@
+(function(ng){
+    
+    var mod = ng.module("calificacionesModule");
+    mod.constant("calificacionesContext", "calificaciones");
+    mod.constant("mensajerosContext", "api/mensajeros");
+    mod.controller('calificacionCtrl', ['$scope', '$http', 'mensajerosContext', '$state', 'calificacionesContext',
+        /**
+         * @ngdoc controller
+         * @name zonas.controller:zonasUpdateCtrl
+         * @description
+         * Definición del controlador auxiliar para actualizar Zonas. 
+         * @param {Object} $scope Referencia injectada al Scope definida para este
+         * controlador, el scope es el objeto que contiene las variables o 
+         * funciones que se definen en este controlador y que son utilizadas 
+         * desde el HTML.
+         * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+         * @param {Object} zonasContext Constante injectada que contiene la ruta
+         * donde se encuentra el API de Editoriales en el Backend.
+         * @param {Object} $state Dependencia injectada en la que se recibe el 
+         * estado actual de la navegación definida en el módulo.
+         */
+        function($scope, $http , mensajerosContext , $state, $rootScope, calificacionesContext)
+        {
+            $rootScope.edit=true;
+            $scope.data={};
+            var idCalificacion=$state.params.calificacionId;
+            /**
+             * @ngdoc function
+             * @name getZonaID
+             * @methodOf zonas.controller:zonasUpdateCtrl
+             * @description
+             * Esta función utiliza el protocolo HTTP para obtener el recurso 
+             * donde se encuentra la editorial por ID en formato JSON.
+             * @param {String} URL Dirección donde se encuentra el recurso
+             * del zona o API donde se puede consultar.
+             */
+            $http.get(mensajerosContext +calificacionesContext+'/'+ idCalificacion).then(function(response)
+            {
+                var calificacion=response.data;
+                $scope.data.id=idCalificacion;
+                $scope.data.latitud=calificacion.calificacion;
+                $scope.data.longitud=calificacion.comentario;
+            });
+            
+            /**
+             * @ngdoc function
+             * @name createZona
+             * @methodOf zonas.controller:ZonaUpdateCtrl
+             * @description
+             * Esta función utiliza el protocolo HTTP para actualizar la zona 
+             * por ID en formato JSON.
+             * @param {String} id El ID de la zona que se va a actualizar.
+             * @param {Object} zona Objeto con la información nueva de la zona.
+             */
+            $scope.updateCalificacion=function()
+            {
+                console.log($scope.data.id);
+                $http.put(mensajerosContext  + $state.params.mensajeroId + calificacionesContext +'/'+idCalificacion,$scope.data).then(function(response)
+                {
+                    $state.go('calificacionesList',{calificacionId:response.data.id},{reload:true});
+                });
+            };
+        }
+    ]);
+    
+})(window.angular);
+
